8453e2b7aaddffe42682d95bf474f926d616c922
Christophe Delage <christ@ophe.net>
2025-08-29 04:26:13 +0200

    fix checkout tool

--- tools/gco-latest.sh
+++ tools/gco-latest.sh
@@ -3,15 +3,17 @@
 set -eo pipefail
 
 # ensure we are in the right repo
-if ! grep -q  'url = git@github.com:chrocapix/utilz\.git' .git/config; then
+if ! grep -q  'url = .*github.com[:/]chrocapix/utilz\.git' .git/config; then
 	print >&2 "error: not in an utilz repo"
 	exit 2
 fi
 
 print "fetching tags..."
+git fetch --all
 git fetch --tags
 
-tag="$(git describe --tags --abbrev=0)"
+tag=$(git describe $(git rev-list --tags --max-count=1))
+# tag="$(git describe --tags --abbrev=0)"
 
 if [[ -z "$tag" ]] then
 	print >&2 "error: no tags"
